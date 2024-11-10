facec7c6f37d7abc171aa2436c10df35705506f3
Kozzzlova <kozlova.jully@gmail.com>
2024-11-10 23:09:45 +0200

    update product page

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,24 @@
-import './swiper-bundle.min';
+// import './swiper-bundle.min';
+// import './swiper';
 import '../scss/index.scss';
 import 'virtual:svg-icons-register';
 import './accordion';
+
+// const swiper = new Swiper('.swiper-container', {
+//    pagination: { el: '.swiper-pagination' },
+//    loop: true,
+// });
+// document.addEventListener('DOMContentLoaded', () => {
+//    const swiper = new Swiper('.swiper-container', {
+//       modules: [Navigation, Pagination],
+//       navigation: {
+//          nextEl: '.swiper-button-next',
+//          prevEl: '.swiper-button-prev',
+//       },
+//       pagination: {
+//          el: '.swiper-pagination',
+//          clickable: true,
+//       },
+//       loop: true,
+//    });
+// });
