b5466ca0751f88da117ca0bf0308e602c087a4e2
Kozzzlova <kozlova.jully@gmail.com>
2024-12-19 12:11:14 +0400

    fixed intl tel inputs

--- src/script/intlTelInput.js
+++ src/script/intlTelInput.js
@@ -3,16 +3,18 @@ import 'intl-tel-input/build/css/intlTelInput.css';
 import '../scss/components/intl-tel-input.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-   const input = document.querySelector('#tel');
-   if (!input) {
-      return; // Выход из функции, если элемент не найден
+   const phoneInputs = document.querySelectorAll('input[type="tel"]');
+   if (phoneInputs.length === 0) {
+      return;
    }
 
-   intlTelInput(input, {
-      initialCountry: 'ua',
-      strictMode: true,
-      separateDialCode: true,
-      utilsScript:
-         'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/19.0.0/js/utils.js',
+   phoneInputs.forEach((input) => {
+      intlTelInput(input, {
+         initialCountry: 'ua',
+         strictMode: true,
+         separateDialCode: true,
+         utilsScript:
+            'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js',
+      });
    });
 });
